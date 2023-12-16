83197c93afea606ef256c4d2df13ba44df5b7c5a
Koranteng <e.s.koranteng@gmail.com>
2023-12-16 09:07:32 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - print code using puts function
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
