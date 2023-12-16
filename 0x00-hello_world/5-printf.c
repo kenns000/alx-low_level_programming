28d151f25da670b3aca2328c441cec5e1df5aae4
Koranteng <e.s.koranteng@gmail.com>
2023-12-16 09:28:00 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+ * Use the function printf
+ * You are not allowed to use the function puts
+ * Your program should return 0
+ * Your program should compile without warning when using the -Wall gcc option
+*/
 int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
