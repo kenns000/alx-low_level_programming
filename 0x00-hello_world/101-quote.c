0d1aad1705fbfcf80bb1cc74b5c84d6a8f798a3e
Koranteng <e.s.koranteng@gmail.com>
2023-12-17 04:34:57 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#incluse <Unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19, 59);
+return (1);
+}
