53e915d1a0ce3903d9922ba511275c335deab3ad
Koranteng <e.s.koranteng@gmail.com>
2023-12-17 01:32:34 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /*
- * Use the function printf
- * You are not allowed to use the function puts
+ *main - function use printf
+ *
  * Your program should return 0
- * Your program should compile without warning when using the -Wall gcc option
+ *
 */
 int main(void)
 {
