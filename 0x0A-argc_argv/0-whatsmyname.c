50bbd56e5ab546b75f3cf529d3f17777aafb661f
Koranteng <e.s.koranteng@gmail.com>
2024-01-13 03:39:29 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -this prints its my name, followed by a new line
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
